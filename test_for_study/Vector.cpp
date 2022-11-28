c8312247e4b2376031ec81d884e7c5dacbca5cb3
Your Name <you@example.com>
2022-11-28 13:55:59 +0300

    idk

--- test_for_study/Vector.cpp
+++ test_for_study/Vector.cpp
@@ -1,13 +1,29 @@
 #include "Vector.hpp"
 
+
+//move constuctor
 template <typename T, typename Alloc>
-T& Vector<T, Alloc>::operator[](size_t idx) {
-	return arr[idx];
+Vector<T, Alloc>::Vector(Vector&& other) : sz(other.sz), arr(other.sz), cap(other.cap), alloc(other.alloc) {
+	other.arr = nullptr;
+	other.sz = 0;
+	other.cap = 0;
 }
 
 template <typename T, typename Alloc>
-Vector<T, Alloc >& Vector<T, Alloc>::operator=(const Vector& copy) {
-	
+Vector<T, Alloc>& Vector<T, Alloc>::operator=(Vector&& other) {
+	Vector<T, Alloc>	new_v = std::move(other);
+	std::swap(arr, other.arr);
+	std::swap(sz, other.sz);
+	std::swap(cap, other.cap);
+	std::swap(alloc, other.alloc);
+	return new_v;
+}
+
+
+
+template <typename T, typename Alloc>
+T& Vector<T, Alloc>::operator[](size_t idx) {
+	return arr[idx];
 }
 
 template <typename T, typename Alloc>
@@ -18,23 +34,33 @@ Vector<T, Alloc>::Vector(size_t sz, const T& value, const Alloc& alloc_) : sz(sz
 		arr[i] = value;
 	}
 }
-
+/*	implement like constuctor because copy not move
 template <typename T, typename Alloc>
 Vector<T, Alloc>::Vector(const Vector& copy) : sz(copy.size()) , cap(copy.capacity()) {
 
 }
-
+*/
 template <typename T, typename Alloc>
 Vector<T, Alloc>::~Vector() {
 	// delete []arr;
-	AllocTraits::deallocate(alloc, arr, sz);
+	AllocTraits::deallocate(alloc, arr, cap);
 }
 
 template <typename T, typename Alloc>
 void	Vector<T, Alloc>::push_back( const T& value ) {
 	
 	if (sz == cap)	reserve(sz * 2);
+	
 	AllocTraits::construct(alloc, arr + sz, value);
+	// new(arr + sz) T(value);
+	++sz;
+}
+
+template <typename T, typename Alloc>
+void	Vector<T, Alloc>::push_back( T&& value ) {
+	if (sz == cap)	reserve(sz * 2);
+	
+	AllocTraits::construct(alloc, arr + sz, std::move(value));
 	// new(arr + sz) T(value);
 	++sz;
 }
@@ -68,7 +94,8 @@ void	Vector<T, Alloc>::reserve( size_t n  ) {
 		for (; i < sz; ++i) {
 			//new(newarr + i) T(arr[i]); // вызов конструктора по данному адресу;
 			//newarr[i] = arr[i]; под newarr лежит reinterpret_cast<T*>(new int8_t[n * sizeof(T)]);
-			AllocTraits::construct(alloc, newarr + i, arr[i]);
+			// AllocTraits::construct(alloc, newarr + i, arr[i]);
+			AllocTraits::construct(alloc, newarr + i, std::move(arr[i]));
 		}
 	} catch(...) {
 		while (i >= 0) {
@@ -91,6 +118,8 @@ void	Vector<T, Alloc>::reserve( size_t n  ) {
 	arr = newarr;
 }
 
+
+
 template <typename T, typename Alloc>
 void	Vector<T, Alloc>::resize( size_t n, const T& value ) {
 	if (n < sz) reserve(cap);
@@ -105,6 +134,38 @@ void	Vector<T, Alloc>::resize( size_t n, const T& value ) {
 			// (arr + i)->~T();
 		}
 	}
+}
+
+template <typename T, typename Alloc>
+Vector<T, Alloc>&	Vector<T, Alloc>::operator=(const Vector<T, Alloc>& other) {
+	if (*this == other)	return *this;
+
+	for (size_t i = 0; i < sz; ++i)  {
+		pop_back();
+	}
+	AllocTraits::deallocate(arr, cap);
+
+	if (AllocTraits::propagate_on_container_copy_assignment::value
+	&& alloc != other.alloc) {
+		alloc = other.alloc;
+	}
+
+	sz = other.sz;
+
+	for (size_t i = 0; i < sz; ++i)  {
+		push_back(other[i]);
+	}
+	return *this;
+}
+
+
+//std::forward
+template <typename T, typename Alloc>
+template <typename...  Args>
+void	Vector<T, Alloc>::emplace_back(Args&&... args) {
+	if (sz == cap) reserve(2 * cap);
+	AllocTraits::construct(alloc, arr + sz, std::forward<Args>(args...));
+	++sz
 }
 
 // vector <bool> работает через _bit_reference
@@ -122,3 +183,4 @@ public:
 
 };
 */
+
--- test_for_study/Vector.hpp
+++ test_for_study/Vector.hpp
@@ -8,6 +8,9 @@
 #include <type_traits>
 #include <algorithm>
 #include <memory>
+#include <utility> // std::move
+
+//rules for class std::move \ move constuctor and move-assignment operator, The rule of five
 
 
 template <typename T, typename Alloc = std::allocator<T>>
@@ -31,15 +34,18 @@ public:
 	void	reserve( size_t n );
 	void	resize( size_t n, const T& value = T() );
 	void	push_back(const T& value);
+	void	push_back( T&& value );
 	void	pop_back( );
-
+	
+	template <typename...  Args>
+	void	emplace_back(Args&&... args);
 	Vector(const Vector& copy);
 	Vector& operator=(const Vector& copy);
+	Vector& operator=(Vector&& copy);
 	T&	operator[](size_t idx);
-
+	
+	Vector(Vector&& other);
 	Vector(size_t sz, const T& value = T(), const Alloc& alloc = Alloc());
 	~Vector();
+
 };
-
-
-
