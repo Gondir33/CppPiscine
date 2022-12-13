ea96d49b8ec63108ebdd71eaea1b020b9ca03ad7
Your Name <you@example.com>
2022-12-13 08:58:36 +0300

    made J

--- yandex1/yandex10.cpp
+++ yandex1/yandex10.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <math.h>
 
 int main() {
 	double a, b, c, d, e, f;
@@ -11,9 +12,21 @@ int main() {
 	} else if (c == 0 && d == 0 && f != 0) {
 		std::cout << '0';
 	} else if (a == 0 && b == 0 && e == 0) {
-		std::cout << '1' << ' ' << - c / d << ' ' << f / d;
+		if (c == 0) {
+			std::cout << '4' << ' ' << f / d;
+		} else if (d == 0) {
+			std::cout << '3' << ' ' << f / c;
+		} else {
+			std::cout << '1' << ' ' << - c / d << ' ' << f / d;
+		}
 	} else if (c == 0 && d == 0 && f == 0) {
-		std::cout << '1' << ' ' << - a / b << ' ' << e / b;
+		if (a == 0) {
+			std::cout << '4' << ' ' << e / b;
+		} else if (b == 0) {
+			std::cout << '3' << ' ' << e / a;
+		} else {
+			std::cout << '1' << ' ' << - a / b << ' ' << e / b;
+		}
 	} else if (a == 0 && c == 0) {
 		if (e / b == f / d)
 			std::cout << '4' << ' ' << e / b;
@@ -24,6 +37,11 @@ int main() {
 			std::cout << '3' << ' ' << e / a;
 		else
 			std::cout << '0';
+	} else if (e == 0 && f == 0) {
+		if (a / c == b / d)
+			std::cout << '1' << ' ' << - a / b << ' ' << '0';
+		else
+			std::cout << '2' << ' ' << '0' << ' ' << '0';
 	} else if (a == 0) {
 		std::cout << '2' << ' ' << (f - d * e / b) / c << ' ' << e / b; 
 	} else if (b == 0) {
@@ -37,6 +55,9 @@ int main() {
 	} else {
 		double x = (f - d * e / b) / (c - d * a / b);
 		double y = (e - a * x) / b;
-		std::cout << '2' << ' ' << x << ' ' << y;
+		if (x == INFINITY || x == -INFINITY || x == NAN || x == -NAN || y == INFINITY || y == -INFINITY || y == NAN || y == -NAN)
+			std::cout << '0';
+		else
+			std::cout << '2' << ' ' << x << ' ' << y;
 	}
 }
