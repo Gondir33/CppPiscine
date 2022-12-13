44146b4bc999da80230f1c4884a7dc56f08f202f
Your Name <you@example.com>
2022-12-13 16:57:56 +0300

    E J

---
+++ yandex2/yandex10.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <vector>
+
+int main() {
+	int n;
+	std::cin >> n;
+	std::vector<double>	v(n);
+}
--- yandex2/yandex7.cpp
+++ yandex2/yandex7.cpp
@@ -12,7 +12,25 @@ void	solution(std::vector<int> v) {
 		min2 = v[1]; min1 = v[0];
 		max2 = v[0]; max1 = v[1];
 	}
-	
+	for (int i = 2; i < v.size(); ++i) {
+		if (max1 < v[i]) {
+			max2 = max1;
+			max1 = v[i];
+		} else if (max2 < v[i]) {
+			max2 = v[i];
+		}
+		if (min1 > v[i]) {
+			min2 = min1;
+			min1 = v[i];
+		} else if (min2 > v[i]) {
+			min2 = v[i];
+		}
+	}
+	if (min1 * min2 > max1 * max2) {
+		std::cout << min1 << ' ' << min2;
+	} else {
+		std::cout << max2 << ' ' << max1;
+	}
 
 
 }
