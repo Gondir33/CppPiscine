c3d9e1be4cad8ecbc55da37efd2ed41e0321ea75
Your Name <you@example.com>
2022-12-17 13:17:05 +0300

    zxc

--- yandex5/yandex8.cpp
+++ yandex5/yandex8.cpp
@@ -1,21 +1,34 @@
 #include <iostream>
+#include <fstream>
 #include <unordered_map>
 
 int main() {
 	int n, k, indexL = 0, indexR = 0;
-	std::cin >> n >> k;
-	std::string	s;
-	std::cin >> s;
+	std::ifstream	in("input.txt");
+	std::string	s, tmp;
+
+	if (in.is_open()) {
+		std::getline(in, s);
+		int j = 0;
+		while (j < s.size() && s[j] != ' ') 	++j;
+		n = std::stoi(s.substr(0, j));
+		++j;
+		k = std::stoi(s.substr(j, s.size() - j));
+		std::getline(in, s);
+	}
+	in.close();
+	
 	std::unordered_map<char, int>	dict;
 	int l = 0;
 	int r = 0;
-	while (r < s.size()) {
+	while (r < n) {
 		++dict[s[r]];
-		if (dict[s[r]] > k) {
-			if (r - l > indexR - indexL) {
+		if (dict[s[r]] <= k) {
+			if (r + 1 - l >= indexR - indexL) {
 				indexL = l;
-				indexR = r;
+				indexR = r + 1;
 			}
+		} else {
 			while (dict[s[r]] > k) {
 				--dict[s[l]];
 				++l;
@@ -23,5 +36,6 @@ int main() {
 		}
 		++r;
 	}
+
 	std::cout << indexR - indexL << ' ' << indexL + 1;
 }
